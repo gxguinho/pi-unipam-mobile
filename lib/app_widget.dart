17521009cbddf7848c12adbb17aac00ab9a2710b
Willian Pereira Silva <79220626+Willi4nn@users.noreply.github.com>
2021-10-07 23:00:19 -0300

    Rotas adicionadas

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:unipam_mobile/shared/themes/app_colors.dart';
-import 'package:unipam_mobile/shared/themes/app_text.dart';
 
 import 'modules/ForgotPassword/ForgotPassword_page.dart';
+import 'modules/ForgotPassword/NewPassword_page.dart';
+import 'modules/ForgotPassword/email_check.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -12,7 +12,12 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Unipam Mobile',
-      home: ForgotPassword(),
+      initialRoute: '/forgotpassword',
+      routes: {
+        '/forgotpassword': (context) => ForgotPassword(),
+        '/emailcheck': (context) => EmailCheck(),
+        '/newpassword': (context) => NewPassword(),
+      },
     );
   }
 }
--- lib/shared/widgets/label_button/label_button.dart
+++ lib/shared/widgets/label_button/label_button.dart
@@ -3,22 +3,25 @@ import 'package:unipam_mobile/shared/themes/app_colors.dart';
 
 class LabelButton extends StatelessWidget {
   final String text;
-  const LabelButton({Key? key, required this.text}) : super(key: key);
+  final String route;
+  const LabelButton({Key? key, required this.text, required this.route})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      child: Text(text),
-      style: ElevatedButton.styleFrom(
-        primary: AppColors.primary,
-        textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-        side: BorderSide(color: Colors.black, width: 1),
-                 minimumSize: Size(400,50),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(5),
+        child: Text(text),
+        style: ElevatedButton.styleFrom(
+          primary: AppColors.primary,
+          textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          side: BorderSide(color: Colors.black, width: 1),
+          minimumSize: Size(400, 50),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5),
+          ),
         ),
-      ),
-      onPressed: () {},
-    );
+        onPressed: () {
+          Navigator.of(context).pushNamed(route);
+        });
   }
 }
